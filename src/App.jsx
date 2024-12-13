8517b48d9d5f5aa7e4dc550a19fb1ebb92c8ae97
Prashant <hadaprashant2002@gmail.com>
2024-12-13 22:41:55 +0530

    update tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 
 function App() {
   return (
-    <div><h1 className="text-3xl font-bold underline text-blue-600">
+    <main>
+      <h1 className="text-3xl font-bold underline text-blue-600">
     Hello world!
-  </h1></div>
+  </h1>
+  <h1 className="text-3xl font-bold font-general underline text-blue-600">
+    Hello world!
+  </h1>
+  <h1 className="text-3xl font-bold font-zentry underline text-blue-600">
+    Hello world!
+  </h1>
+  <h1 className="text-3xl font-bold font-circular-web underline text-blue-600">
+    Hello world!
+  </h1>
+  <h1 className="text-3xl font-bold font-robert-medium underline text-blue-600">
+    Hello world!
+  </h1>
+  <h1 className="text-3xl font-bold font-robert-regular underline text-blue-600">
+    Hello world!
+  </h1>
+  </main>
   )
 }
 
