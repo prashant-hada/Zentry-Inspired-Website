187ef4f3582ba8cddf2939c0eac106eea6312610
Prashant <hadaprashant2002@gmail.com>
2024-12-29 23:42:52 +0530

    added fonts and color schemes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
+import Hero  from './components/Hero'
 
 function App() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline text-blue-600">
-    Hello world!
-  </h1>
-  <h1 className="text-3xl font-bold font-general underline text-blue-600">
-    Hello world!
-  </h1>
-  <h1 className="text-3xl font-bold font-zentry underline text-blue-600">
-    Hello world!
-  </h1>
-  <h1 className="text-3xl font-bold font-circular-web underline text-blue-600">
-    Hello world!
-  </h1>
-  <h1 className="text-3xl font-bold font-robert-medium underline text-blue-600">
-    Hello world!
-  </h1>
-  <h1 className="text-3xl font-bold font-robert-regular underline text-blue-600">
-    Hello world!
-  </h1>
+  //   <main>
+  //     <h1 className="text-3xl font-bold underline text-blue-600">
+  //   Hello world!
+  // </h1>
+  // <h1 className="text-3xl font-bold font-general underline text-blue-600">
+  //   Hello world!
+  // </h1>
+  // <h1 className="text-3xl font-bold font-zentry underline text-blue-600">
+  //   Hello world!
+  // </h1>
+  // <h1 className="text-3xl font-bold font-circular-web underline text-blue-600">
+  //   Hello world!
+  // </h1>
+  // <h1 className="text-3xl font-bold font-robert-medium underline text-blue-600">
+  //   Hello world!
+  // </h1>
+  // <h1 className="text-3xl font-bold font-robert-regular underline text-blue-600">
+  //   Hello world!
+  // </h1>
+  // </main>
+  <main className='relative min-h-screen w-screen overflow-x-hidden'>
+    <Hero />
   </main>
   )
 }
