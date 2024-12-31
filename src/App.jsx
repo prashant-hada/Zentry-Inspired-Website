84ebad994a7552fcca5d56aa89ad4baf35665da3
Prashant <hadaprashant2002@gmail.com>
2024-12-31 23:59:10 +0530

    build the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,6 @@ import Navbar from './components/Navbar'
 
 function App() {
   return (
-  //   <main>
-  //     <h1 className="text-3xl font-bold underline text-blue-600">
-  //   Hello world!
-  // </h1>
-  // <h1 className="text-3xl font-bold font-general underline text-blue-600">
-  //   Hello world!
-  // </h1>
-  // <h1 className="text-3xl font-bold font-zentry underline text-blue-600">
-  //   Hello world!
-  // </h1>
-  // <h1 className="text-3xl font-bold font-circular-web underline text-blue-600">
-  //   Hello world!
-  // </h1>
-  // <h1 className="text-3xl font-bold font-robert-medium underline text-blue-600">
-  //   Hello world!
-  // </h1>
-  // <h1 className="text-3xl font-bold font-robert-regular underline text-blue-600">
-  //   Hello world!
-  // </h1>
-  // </main>
   <main className='relative min-h-screen w-screen overflow-x-hidden'>
     <Navbar />
     <Hero />
