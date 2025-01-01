1d780cde505423f21922e0391f11cb3464a85ec0
Prashant <hadaprashant2002@gmail.com>
2025-01-02 00:00:51 +0530

    contact page created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About"
 import Navbar from './components/Navbar'
 import Features from './components/Features'
 import Story from './components/Story'
+import Contact from './components/Contact'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
     <About />
     <Features />
     <Story />
+    <Contact />
     <section className='z-0 bg-blue-300 min-h-screen' />
   </main>
   )
